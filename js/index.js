577439c3ad9693c53cd727e831e36bb31e433941
Jaehooni <50170667+Jaehooni@users.noreply.github.com>
2021-08-31 12:16:39 +0000

    Add EventListener to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log("hello!")
+const loading = document.querySelector('.loading');
+const startPageWrapper = document.querySelector('.start-page-wrapper');
+const startButton = document.querySelector('.start-page-option h2:nth-child(1)')
+
+const entranceEffect = function(){
+    startPageWrapper.classList.toggle('blur');
+    loading.classList.toggle('invisible');
+}
+
+const temp = function(){
+    startPageWrapper.classList.toggle('blur');
+    loading.classList.toggle('invisible');
+}
+
+startButton.addEventListener('click', entranceEffect);
+loading.addEventListener('click', temp);
+
