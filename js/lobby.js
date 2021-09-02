08cd49a4ffd7a584e9b427f2230a956ba62ecb6f
Jaehooni <50170667+Jaehooni@users.noreply.github.com>
2021-09-02 14:13:11 +0000

    Update lobby.html

--- js/lobby.js
+++ js/lobby.js
@@ -1,14 +1,14 @@
-const gameIcons = document.querySelectorAll(".lobby-screen__games li");
+// const gameIcons = document.querySelectorAll(".lobby-screen__games li");
 
-const showGameName = function(){
-  this.lastElementChild.classList.remove("none");
-}
+// const showGameName = function(){
+//   this.lastElementChild.classList.remove("none");
+// }
 
-const hideGameName = function(){
-  this.lastElementChild.classList.add("none");
-}
+// const hideGameName = function(){
+//   this.lastElementChild.classList.add("none");
+// }
 
-for (let gameIcon of gameIcons){
-  gameIcon.addEventListener('mousemove', showGameName);
-  gameIcon.addEventListener('mouseleave', hideGameName)
-}
+// for (let gameIcon of gameIcons){
+//   gameIcon.addEventListener('mousemove', showGameName);
+//   gameIcon.addEventListener('mouseleave', hideGameName)
+// }
