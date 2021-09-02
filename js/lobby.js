de9de2abd13660e5891ff0b8f534158759f7fe26
Jaehooni <50170667+Jaehooni@users.noreply.github.com>
2021-09-02 12:28:59 +0000

    Modify lobby.html, lobby.css, lobby.js

---
+++ js/lobby.js
@@ -0,0 +1,14 @@
+const gameIcons = document.querySelectorAll(".lobby-screen__games li");
+
+const showGameName = function(){
+  this.lastElementChild.classList.remove("none");
+}
+
+const hideGameName = function(){
+  this.lastElementChild.classList.add("none");
+}
+
+for (let gameIcon of gameIcons){
+  gameIcon.addEventListener('mousemove', showGameName);
+  gameIcon.addEventListener('mouseleave', hideGameName)
+}
